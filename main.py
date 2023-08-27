232029164f9c1f8c33adc82195d48cefbd012ad0
Evan Mattiza <emattiza@gmail.com>
2023-08-27 18:07:41 +0000

    progress update: end of class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """
+$ ls main.py | entr pytest
 Wiring: Simulating Digital Logic with Python
 
 Shortened material from day 1 of a course with David Beazley
@@ -29,7 +30,6 @@ Wire(1)
 
 """
 
-
 def test_basic_wire():
   """
   >>> a = Wire()
@@ -45,8 +45,35 @@ class Wire:
   """
   Implement your wire here!
   """
-  ...
+  def __init__(self, x=0):
+    self._value = x
+    self.watchers = set()
 
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, new_value):
+    # in place tuple reassignment
+    # old_value = self._value
+    # self._value = new_value
+    old_value = self._value
+    self._value = new_value
+    if new_value != old_value:
+      self.notify()
+
+  def notify(self):
+    for watcher in self.watchers:
+      watcher()
+      
+
+  def watch(self, func):
+    self.watchers.add(func)
+    func()
+    
+  def __repr__(self):
+    return f'Wire({self.value})'
 
 """
 --------------------
@@ -69,26 +96,28 @@ See and uncomment test_basic_connection for an example repl session
 """
 
 
-def connect(a, b):
+def connect(a: Wire, b: Wire):
+  a.watch(lambda: setattr(b, "value", a.value))
+  b.watch(lambda: setattr(a, "value", b.value))
+
+
+def test_basic_connection():
+  """
+  >>> a = Wire()
+  >>> b = Wire()
+  >>> connect(a, b)
+  >>> a.value = 1
+  >>> a
+  Wire(1)
+  >>> b
+  Wire(1)
+  >>> b.value = 0
+  >>> a
+  Wire(0)
+  >>> b
+  Wire(0)
+  """
   ...
-
-
-# def test_basic_connection():
-#   """
-#   >>> a = Wire()
-#   >>> b = Wire()
-#   >>> connect(a, b)
-#   >>> a.value = 1
-#   >>> a
-#   Wire(0)
-#   >>> b
-#   Wire(1)
-#   >>> b.value = 0
-#   >>> a
-#   Wire(0)
-#   >>> b
-#   Wire(0)
-#   """
 """
 The gate and a single operation
 
@@ -100,26 +129,46 @@ def nor3(a, b, c):
   return not (a or b or c)
 
 
-# class NOR3:
+class NOR3:
+  def __init__(self, a: Wire, b: Wire, c: Wire, out: Wire):
+    """1 if all are 0, else 0"""
+    self.a = a
+    self.b = b
+    self.c = c
+    self.out = out
+    a.watch(self.update)
+    b.watch(self.update)
+    c.watch(self.update)
+
+
+  def update(self):
+    self.out.value = int(nor3(self.a.value, self.b.value, self.c.value))
+    
+  def __repr__(self):
+    return f"{self.__class__.__name__}({self.a.value}, {self.b.value}, {self.c.value}, {self.out.value})"
+  
+
+
 # or
 # def NOR3()
 """
 Here is a sample repl Session utilizing this NOR3 gate
 """
 
-# def test_nor3_gate():
-#   """
-#   >>> a = Wire()
-#   >>> b = Wire()
-#   >>> c = Wire()
-#   >>> out = Wire()
-#   >>> NOR3(a, b, c, out)
-#   >>> out.value
-#   1
-#   >>> a.value = 1
-#   >>> out.value
-#   0
-#   """
+def test_nor3_gate():
+  """
+  >>> a = Wire()
+  >>> b = Wire()
+  >>> c = Wire()
+  >>> out = Wire()
+  >>> NOR3(a, b, c, out)
+  NOR3(0, 0, 0, 1)
+  >>> out.value
+  1
+  >>> a.value = 1
+  >>> out.value
+  0
+  """
 """
 Other Logic Gates
 
@@ -156,17 +205,130 @@ XOR(x, y, out)      x       y      out
 """
 
 
-def NOT():
-  ...
+class NOT:
+  def __init__(self, a, out):
+    self.a = a
+    self.b = Wire()
+    self.c = Wire()
+    self.out = out
+    NOR3(self.a, self.b, self.c, self.out)
+
+  def __repr__(self):
+    return f"NOT({self.a.value}, {self.out.value})"
+
+def test_not_gate():
+  """
+  >>> a = Wire()
+  >>> out = Wire()
+  >>> NOT(a, out)
+  NOT(0, 1)
+  >>> out.value
+  1
+  >>> a.value = 1
+  >>> out.value
+  0
+  """
+
+class AND:
+  def __init__(self, a, b, out):
+    self.a = a
+    self.b = b
+    self.c = Wire()
+    self.out = out
+
+    self.aa = Wire()
+    NOR3(self.a, self.a, self.c, self.aa)
+    self.bb = Wire()
+    NOR3(self.b, self.b, self.c, self.bb)
+
+    NOR3(self.aa, self.bb, self.c, self.out)
+
+  def __repr__(self):
+    return f"AND({self.a.value}, {self.b.value}, {self.out.value})"
+  
+class OR:
+  def __init__(self, a, b, out):
+    self.a = a
+    self.b = b
+    self.out = out    
+    a.watch(self.update)
+    b.watch(self.update)
+
+  def update(self):
+    self.out.value = int(nor3(nor3(self.a.value, self.b.value, 0), nor3(self.a.value, self.b.value, 0), 0))
+
+  def casey_cheated(self):
+    a_gate_out = Wire()
+    b_gate_out = Wire()
+    NOR3(self.a, self.a, Wire(), a_gate_out)
+    NOR3(self.b, self.b, Wire(), b_gate_out)
+    NOR3(a_gate_out, b_gate_out, Wire(), self.out)
+
+  def __repr__(self):
+    return f"OR({self.a.value}, {self.b.value}, {self.out.value})"
+
+def test_or_gate():
+  """
+  >>> a = Wire()
+  >>> b = Wire()
+  >>> out = Wire()
+  >>> OR(a, b, out)
+  OR(0, 0, 0)
+  >>> out.value
+  0
+  >>> a.value = 1
+  >>> b.value = 0
+  >>> out.value
+  1
+  >>> a.value = 0
+  >>> b.value = 1
+  >>> out.value
+  1
+  >>> a.value = 1
+  >>> b.value = 1
+  >>> out.value
+  1
+  """
+
+class XOR():
+  def __init__(self, a, b, out):
+    self.a = a
+    self.b = b
+    self.out = out
+
+    a_nor_a = Wire()
+    NOR3(self.a, self.a, Wire(), a_nor_a)
+    b_nor_b = Wire()
+    NOR3(self.b, self.b, Wire(), b_nor_b)
+    aa_nor_bb = Wire()
+    NOR3(a_nor_a, b_nor_b, Wire(), aa_nor_bb)
+    a_nor_b = Wire()
+    NOR3(self.a, self.b, Wire(), a_nor_b)
+    NOR3(aa_nor_bb, a_nor_b, Wire(), self.out)
+  
+  def __repr__(self):
+    return f"XOR({self.a.value}, {self.b.value}, {self.out.value})"
 
 
-def AND():
-  ...
-
-
-def OR():
-  ...
-
-
-def XOR():
-  ...
+def test_xor_gate():
+  """
+  >>> a = Wire()
+  >>> b = Wire()
+  >>> out = Wire()
+  >>> XOR(a, b, out)
+  XOR(0, 0, 0)
+  >>> out.value
+  0
+  >>> a.value = 1
+  >>> b.value = 0
+  >>> out.value
+  1
+  >>> a.value = 0
+  >>> b.value = 1
+  >>> out.value
+  1
+  >>> a.value = 1
+  >>> b.value = 1
+  >>> out.value
+  0
+  """
